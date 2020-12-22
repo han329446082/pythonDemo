7a71ef514976f5715a9333c6e5ca1cbbd92dfa6f
kara <329446082@qq.com>
2020-12-22 15:23:01 +0800

    test by kara

---
+++ testcases/log/test1.py
@@ -0,0 +1,8 @@
+from util import util
+
+logger = util.get_logger()
+logger.info('info message')
+logger.debug('debug message')
+logger.warning('warning message')
+logger.error('error message')
+logger.critical('critical message')
